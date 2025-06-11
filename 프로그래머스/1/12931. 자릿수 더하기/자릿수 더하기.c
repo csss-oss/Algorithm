9f2cb6cb61fbdb849c8608a2e49e752bf1dc07e0
csss-oss <kjhje9352@naver.com>
2025-06-11 21:27:54 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.c
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.c
@@ -7,18 +7,12 @@ int solution(int n) {
     //숫자를 입력받아서 문자열로 만든다음 각각 자릿수를 배열로 쪼개서 그 쪼갠걸 다시 숫자로 만들어서 더하자 
     int temp = n;
     int count =0;
-    while(temp > 0)
+    while(temp)
     {
+        count += (temp % 10);
         temp = temp / 10;
-        count++;
+        
     }
-    
-   char buf[100];
-   sprintf(buf,"%d",n);
-  
-    for(int i = 0; i < count; i++)
-    {
-        answer += (buf[i] -'0');
-    }
+    answer = count;
     return answer;
 }
