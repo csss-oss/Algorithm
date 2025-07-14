8b50c35fe4a1e402376d63e4e33f63d9ad4a3b39
csss-oss <kjhje9352@naver.com>
2025-07-14 18:40:24 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.07 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.c
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.c
@@ -1,37 +1,17 @@
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-
 // 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
 int solution(const char* t, const char* p) {
     int answer = 0;
-    int N = strlen(t);
-    int M = strlen(p);
-    int same = 0;
-    int k = 0;
-    
-    for(int i = 0 ; i <= N-M ;i++){
-        k = 0;
-        same = 0 ;
-        for(int j = i ; j < i+M ;j++){
-            if(t[j] < p[k]){
-                answer ++;
-                break;
-            }
-            else if (t[j] > p[k]){
-                break;
-            }
-            else if (t[j] == p[k]){
-                same ++;
-            }
-            k++;
-        }
-        
-        if(same == M){
-            printf("%d\n",same);
-            answer ++;
-        }
+    for(int i = 0; i <= strlen(t) - strlen(p); i++)
+    {
+        if(strncmp(t + i, p, strlen(p)) <= 0)
+            answer++;//t는 문자열 시작의 주소인데
+        //+1을 한다면 주소 +1 즉,예를들어 t가"abcd"이면
+        //t+1 = "bcd"로 생각
     }
     return answer;
 }
